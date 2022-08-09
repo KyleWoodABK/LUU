67ef0feaddf0138d3aef2bfacf4d8718922aa308
Kyle Wood <kyle.wood@activision.com>
2022-08-09 09:51:00 -0700

    w5d2 networking lesson continued

--- Week 5/w5d1 NetworkingENet lesson/w5d1 NetworkingENet lesson/w5d1 NetworkingENet lesson.cpp
+++ Week 5/w5d1 NetworkingENet lesson/w5d1 NetworkingENet lesson/w5d1 NetworkingENet lesson.cpp
@@ -57,6 +57,34 @@ int main(int argc, char** argv)
                 "An error occurred while trying to create an ENet server host.\n");
             exit(EXIT_FAILURE);
         }
+
+        while (1)
+        {
+            ENetEvent event;
+            /* Wait up to 1000 milliseconds for an event. */
+            while (enet_host_service(server, &event, 1000) > 0)
+            {
+                switch (event.type)
+                {
+                case ENET_EVENT_TYPE_CONNECT:
+                    cout << "A new client connected from " << event.peer->address.host << ":" << event.peer->address.port << "." << endl;
+                    /* Store any relevant client information here. */
+                    event.peer->data = (void*)("Client information");
+                    break;
+                case ENET_EVENT_TYPE_RECEIVE:
+                    cout << "A packet of length " << event.packet->dataLength << " containing " << (char*)event.packet->data << "was received from " << (char*)event.peer->data << "on channel " << event.channelID << "." << endl;
+                    /* Clean up the packet now that we're done using it. */
+                    enet_packet_destroy(event.packet);
+
+                    break;
+
+                case ENET_EVENT_TYPE_DISCONNECT:
+                    cout << (char*)event.peer->data << " disconnected." << endl;
+                    /* Reset the peer's client information. */
+                    event.peer->data = NULL;
+                }
+            }
+        } 
     }
     else if (UserInput == 2)
     {
@@ -65,6 +93,36 @@ int main(int argc, char** argv)
             fprintf(stderr,
                 "An error occurred while trying to create an ENet client host.\n");
             exit(EXIT_FAILURE);
+        }
+
+        ENetAddress address;
+        ENetEvent event;
+        ENetPeer* peer;
+        /* Connect to some.server.net:1234. */
+        enet_address_set_host(&address, "127.0.0.1");
+        address.port = 1234;
+        /* Initiate the connection, allocating the two channels 0 and 1. */
+        peer = enet_host_connect(client, &address, 2, 0);
+        if (peer == NULL)
+        {
+            fprintf(stderr,
+                "No available peers for initiating an ENet connection.\n");
+            exit(EXIT_FAILURE);
+        }
+        /* Wait up to 5 seconds for the connection attempt to succeed. */
+        if (enet_host_service(client, &event, 5000) > 0 &&
+            event.type == ENET_EVENT_TYPE_CONNECT)
+        {
+            cout<<"Connection to some.server.net:1234 succeeded."<<endl;
+            
+        }
+        else
+        {
+            /* Either the 5 seconds are up or a disconnect event was */
+            /* received. Reset the peer in the event the 5 seconds   */
+            /* had run out without any significant event.            */
+            enet_peer_reset(peer);
+            cout<<"Connection to 127.0.0.1:1234 failed."<<endl;
         }
     }
     else
